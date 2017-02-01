fc1a954feb14738d851850b908be332796b657d8
Ari Falkner <arifalkner.eng@gmail.com>
2017-01-31 22:01:46 -0800

    add message handler

--- chatbot_handler.py
+++ chatbot_handler.py
@@ -2,8 +2,15 @@ from flask import request, Flask
 
 app = Flask(__name__)
 
-@app.route('/receive')
+token = 'EAAZAN5XZBKMSUBABWAvzebHZB3myopwucptBEaKmBgaLdam9aFpC2d4rb7QiRiI3wY7Hr8WjLYECPc4qXRATodacvOxS1ZB7g4XRq0SNbVhoZB71l5phRZAJ4v2eOR9GxhxjBDfBZA4OtKOpCikbSPaLLqsOCazkaoPmZAHZAutyFKAZDZD'
+
+@app.route('/receive', methods=['GET'])
 def serve():
     if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == 'koushik':
         return request.args.get('hub.challenge')
     return 'fuck_you'
+
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    print request.body.__dict__
